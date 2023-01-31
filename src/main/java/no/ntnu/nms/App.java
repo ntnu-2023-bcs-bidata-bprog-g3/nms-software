e4c47932def773ac763c42b67e9edc9ef8bac64b
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-01-31 10:32:23 +0100

    Added call to start API from Application class

--- src/main/java/no/ntnu/nms/App.java
+++ src/main/java/no/ntnu/nms/App.java
@@ -1,8 +1,18 @@
 package no.ntnu.nms;
 
+import no.ntnu.nms.api.ApiApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        // Starts the API WebServer and hosts the API from the ApiApp class.
+        ApplicationContext ctx = SpringApplication.run(ApiApp.class, args);
+        System.out.println("Application name: " + ctx.getDisplayName());
+
     }
 }
