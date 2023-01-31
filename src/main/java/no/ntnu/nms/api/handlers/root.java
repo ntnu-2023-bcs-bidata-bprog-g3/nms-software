138a6bdfa785ac3a958bd8f035e04b476a9127ab
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-01-31 10:32:42 +0100

    Added initial RestController with handlers for testing purposes

---
+++ src/main/java/no/ntnu/nms/api/handlers/root.java
@@ -0,0 +1,18 @@
+package no.ntnu.nms.api.handlers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = {"/"})
+public class root {
+
+    @GetMapping(value={""})
+    public String rootGetter() {
+        return "This is the root handler";
+    }
+
+    @PostMapping(value={""})
+    public String rootPost(@RequestBody String postString) {
+        return "You posted: " + postString;
+    }
+}
