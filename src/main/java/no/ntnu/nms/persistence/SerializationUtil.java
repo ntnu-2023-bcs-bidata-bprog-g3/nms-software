f0414f11c96f1fa444441e448d8b85021da799b9
ivannorderhaug <norderhaug@protonmail.com>
2023-02-01 15:49:12 +0100

    added descriptive comments to SerializationUtil

--- src/main/java/no/ntnu/nms/persistence/SerializationUtil.java
+++ src/main/java/no/ntnu/nms/persistence/SerializationUtil.java
@@ -6,9 +6,17 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import no.ntnu.nms.domain_model.PoolRegistry;
-
+/**
+ * Utility class for serializing and deserializing PoolRegistry objects.
+ */
 public class SerializationUtil {
-    
+
+        /**
+         * Serializes the given PoolRegistry object to the given file.
+         * @param registry The PoolRegistry object to serialize.
+         * @param file The file to serialize to.
+         * @throws IOException
+         */
         public static void serialize(PoolRegistry registry, String file) throws IOException {
             FileOutputStream fos = new FileOutputStream(file);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -17,6 +25,13 @@ public class SerializationUtil {
             fos.close();
         }
 
+        /**
+         * Deserializes a PoolRegistry object from the given file.
+         * @param file The file to deserialize from.
+         * @return The deserialized PoolRegistry object.
+         * @throws IOException
+         * @throws ClassNotFoundException
+         */
         public static PoolRegistry deserialize(String file) throws IOException, ClassNotFoundException {
             FileInputStream fis = new FileInputStream(file);
             ObjectInputStream ois = new ObjectInputStream(fis);
