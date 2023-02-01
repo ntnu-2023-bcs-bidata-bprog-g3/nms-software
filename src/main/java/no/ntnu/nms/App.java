2ca10b58fda337e24518f195f291d8e5ec6e3e8f
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-01 15:45:31 +0100

    Added test of pool and pool registry in main for api testing

--- src/main/java/no/ntnu/nms/App.java
+++ src/main/java/no/ntnu/nms/App.java
@@ -1,5 +1,7 @@
 package no.ntnu.nms;
 
+import no.ntnu.nms.domain_model.Pool;
+import no.ntnu.nms.domain_model.PoolRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,5 +15,11 @@ public class App {
         // Starts the API WebServer and hosts the API from the ApiApp class.
         ApplicationContext ctx = SpringApplication.run(App.class, args);
         System.out.println("Application name: " + ctx.getDisplayName());
+
+        PoolRegistry poolreg = PoolRegistry.getInstance();
+        poolreg.addPool(new Pool("test1"));
+        poolreg.addPool(new Pool("test2"));
+        poolreg.addPool(new Pool("test3"));
+
     }
 }
