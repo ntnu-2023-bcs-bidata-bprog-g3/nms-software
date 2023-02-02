3d4c58278f239116d536797ab997659f14543063
ivannorderhaug <norderhaug@protonmail.com>
2023-02-02 14:04:44 +0100

    made initial cryptography class

---
+++ src/main/java/no/ntnu/nms/security/Cryptography.java
@@ -0,0 +1,12 @@
+package no.ntnu.nms.security;
+
+import org.springframework.util.SerializationUtils;
+
+import no.ntnu.nms.domain_model.PoolRegistry;
+
+/*
+ * Utility class for encrypting and decrypting data.
+ */
+public class Cryptography {
+
+}
