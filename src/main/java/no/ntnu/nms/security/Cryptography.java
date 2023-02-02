bcb327be3d886b993d0219b252e3890ddcc80992
ivannorderhaug <norderhaug@protonmail.com>
2023-02-02 14:06:36 +0100

    implemented XOR encryption for byte arrays with key

--- src/main/java/no/ntnu/nms/security/Cryptography.java
+++ src/main/java/no/ntnu/nms/security/Cryptography.java
@@ -9,4 +9,18 @@ import no.ntnu.nms.domain_model.PoolRegistry;
  */
 public class Cryptography {
 
+    /**
+     * XORs a byte array with a key.
+     * @param input The byte array to XOR.
+     * @param key The key to XOR with.
+     * @return The XORed byte array.
+     * @see PoolRegistry
+     */
+    private static byte[] xorWithKey(byte[] input, byte[] key) {
+        byte[] output = new byte[input.length];
+        for (int i = 0; i < input.length; i++) {
+            output[i] = (byte) (input[i] ^ key[i % key.length]);
+        }
+        return output;
+    }
 }
