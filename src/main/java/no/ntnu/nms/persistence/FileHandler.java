4006283251bebec6656b4cf65208ad8040fa7334
ivannorderhaug <norderhaug@protonmail.com>
2023-02-02 14:08:36 +0100

    implemented initial FIleHandler skeleton

---
+++ src/main/java/no/ntnu/nms/persistence/FileHandler.java
@@ -0,0 +1,11 @@
+package no.ntnu.nms.persistence;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+/**
+ * Utility class for serializing and deserializing PoolRegistry objects.
+ */
+public class FileHandler {
+}
