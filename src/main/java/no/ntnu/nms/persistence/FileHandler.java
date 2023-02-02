c96a0efaaf8b3dfe315b314b3fb1d223e9fe251a
ivannorderhaug <norderhaug@protonmail.com>
2023-02-02 14:10:45 +0100

    implemented readFromFile method

--- src/main/java/no/ntnu/nms/persistence/FileHandler.java
+++ src/main/java/no/ntnu/nms/persistence/FileHandler.java
@@ -8,6 +8,7 @@ import java.io.ObjectOutputStream;
  * Utility class for serializing and deserializing PoolRegistry objects.
  */
 public class FileHandler {
+
     /**
      * Writes a byte array to a file.
      * @param data The byte array to write.
@@ -21,6 +22,24 @@ public class FileHandler {
             fileOut.close();
         } catch (IOException i) {
             return;
+        }
+    }
+    /**
+     * Reads a byte array from a file.
+     * @return The byte array read from the file.
+     */
+    public static byte[] readFromFile() {
+        try {
+            FileInputStream fileIn = new FileInputStream("poolreg.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            byte[] data = (byte[]) in.readObject();
+            in.close();
+            fileIn.close();
+            return data;
+        } catch (IOException i) {
+            return null;
+        } catch (ClassNotFoundException c) {
+            return null;
         }
     }
 }
