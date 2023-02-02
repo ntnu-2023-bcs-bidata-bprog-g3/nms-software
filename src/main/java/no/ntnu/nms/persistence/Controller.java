793fb996b7ea908d6bbb894282f9479c4b0ffb1f
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-02 14:24:02 +0100

    Filled Controller class

--- src/main/java/no/ntnu/nms/persistence/Controller.java
+++ src/main/java/no/ntnu/nms/persistence/Controller.java
@@ -1,5 +1,28 @@
 package no.ntnu.nms.persistence;
 
+import no.ntnu.nms.domain_model.PoolRegistry;
+import no.ntnu.nms.security.Cryptography;
+import no.ntnu.nms.security.KeyGenerator;
+
 public class Controller {
+
+    /**
+     * Saves a PoolRegistry object to file.
+     * @param poolreg The PoolRegistry object to save.
+     * @return True if the PoolRegistry object was saved successfully, false otherwise.
+     */
+    public boolean savePoolReg(PoolRegistry poolreg) {
+        byte[] encrypted = Cryptography.encrypt(poolreg, KeyGenerator.generateKey());
+        return FileHandler.writeToFile(encrypted);
+    }
+
+    /**
+     * Loads a PoolRegistry object from file.
+     * @return The PoolRegistry object loaded from file.
+     */
+    public PoolRegistry loadPoolReg() {
+        byte[] encrypted = FileHandler.readFromFile();
+        return Cryptography.decrypt(encrypted, KeyGenerator.generateKey());
+    }
 
 }
