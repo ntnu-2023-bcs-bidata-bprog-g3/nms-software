5b593baf50761c1ba574129ceaf5b33599f98fc6
Sander Osvik Brekke <73897453+sanderob@users.noreply.github.com>
2023-02-03 14:32:08 +0100

    GitHub actions final (#2)
    * Added workflow
    * Added placeholder test

---
+++ src/test/java/InitialTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InitialTest {
+
+    @Test
+    public void placeholderTest() {
+        assertTrue(true);
+    }
+}
