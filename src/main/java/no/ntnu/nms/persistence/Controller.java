f2e4e28d57729266da220cc33ab324503001592a
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-05 00:37:57 +0100

    Added public methods for utilizing the class functionality

--- src/main/java/no/ntnu/nms/persistence/Controller.java
+++ src/main/java/no/ntnu/nms/persistence/Controller.java
@@ -19,6 +19,55 @@ public class Controller {
     public static final String POOL_REGISTRY_FILE_CHECKSUM_PATH = "poolreg.ser.md5";
 
     /**
+     * Saves a PoolRegistry object to file and calculates a checksum, in addition to writing the
+     * checksum to file.
+     * @param poolreg {@link PoolRegistry} The PoolRegistry object to save.
+     * @return {@link Boolean} True if the PoolRegistry object was saved successfully,
+     * false otherwise.
+     */
+    public static boolean savePoolRegAndChecksum(PoolRegistry poolreg) {
+        deletePoolReg();    // Deletes the old pool file. If it is deleted, but failed to write new,
+                            // the old would be outdated anyway
+        boolean success = savePoolReg(poolreg);
+        if (success) {
+            try {
+                String checksum = Checksum.checksum(POOL_REGISTRY_FILE_DIRECTORY_PATH);
+                success = FileHandler.writeToFile(Cryptography.encrypt(checksum.getBytes(),
+                        KeyGenerator.generateKey()), POOL_REGISTRY_FILE_CHECKSUM_PATH);
+            } catch (RuntimeException e) {
+                return false;
+            }
+        }
+        if (!success) {
+            // TODO: ADD LOGGING!!!!
+            System.out.println("Major important error!");
+        }
+        return success;
+    }
+
+    /**
+     * Loads a PoolRegistry object from file and checks the checksum.
+     * @return {@link PoolRegistry} The PoolRegistry object loaded from file, null if failed.
+     */
+    public static PoolRegistry loadPoolRegAndCheckChecksum() {
+        PoolRegistry poolreg = loadPoolReg();
+        if (poolreg != null) {
+            try {
+                String checksum = Checksum.checksum(POOL_REGISTRY_FILE_DIRECTORY_PATH);
+                byte[] encryptedChecksum = FileHandler.readFromFile(POOL_REGISTRY_FILE_CHECKSUM_PATH);
+                String checksumFromFile = new String(Cryptography.decryptBytes(encryptedChecksum,
+                        KeyGenerator.generateKey()));
+                if (!checksum.equals(checksumFromFile)) {
+                    return null;
+                }
+            } catch (RuntimeException e) {
+                return null;
+            }
+        }
+        return poolreg;
+    }
+
+    /**
      * Saves a PoolRegistry object to file.
      * @param poolreg The PoolRegistry object to save.
      * @return True if the PoolRegistry object was saved successfully, false otherwise.
