a3258486f400c39fdfa28ae40a1b3c7cad5215a0
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-06 14:49:29 +0100

    Added SpringBootApplication tag

--- src/main/java/no/ntnu/nms/App.java
+++ src/main/java/no/ntnu/nms/App.java
@@ -2,14 +2,14 @@ package no.ntnu.nms;
 
 import no.ntnu.nms.domain_model.Pool;
 import no.ntnu.nms.domain_model.PoolRegistry;
-import no.ntnu.nms.domain_model.Pool;
-import no.ntnu.nms.domain_model.PoolRegistry;
 import no.ntnu.nms.logging.Logging;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import java.io.IOException;
 
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
