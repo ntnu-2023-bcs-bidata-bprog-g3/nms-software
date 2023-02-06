98299ad2fe64b56eb6677b697dec97812e877b5f
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-06 13:00:04 +0100

    Undo joke statement

--- src/main/java/no/ntnu/nms/security/Checksum.java
+++ src/main/java/no/ntnu/nms/security/Checksum.java
@@ -54,7 +54,10 @@ public class Checksum {
         String checksum = Checksum.getChecksumFromFile(fileDirectory);
         byte[] decryptedChecksumFromFile = Cryptography.decryptBytes(FileHandler
                 .readFromFile(checksumDirectory), KeyGenerator.KEY);
-        return (!(decryptedChecksumFromFile == null || checksum == null)
-                && checksum.equals(new String(decryptedChecksumFromFile)));
+        if (decryptedChecksumFromFile == null || checksum == null) {
+            return false;
+        }
+        String checksumFromFile = new String(decryptedChecksumFromFile);
+        return checksum.equals(checksumFromFile);
     }
 }
