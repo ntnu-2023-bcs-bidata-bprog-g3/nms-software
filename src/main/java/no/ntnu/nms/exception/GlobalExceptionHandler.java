8098d6b2f9fefda0762aff80102ae4b8469ca96e
ivannorderhaug <norderhaug@protonmail.com>
2023-02-10 12:04:18 +0100

    update GlobalExceptionHandler with skeleton exception handlers

--- src/main/java/no/ntnu/nms/exception/GlobalExceptionHandler.java
+++ src/main/java/no/ntnu/nms/exception/GlobalExceptionHandler.java
@@ -1,4 +1,41 @@
 package no.ntnu.nms.exception;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.io.IOException;
+
+/**
+ * Exception class for FileHandler.
+ */
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    /**
+     * Constructor for FileHandlerException.
+     * @param  e {@link FileHandlerException} the exception.
+     * @return message {@link String} the error message.
+     */
+    @ExceptionHandler(FileHandlerException.class)
+    public String handleFileHandlerException(FileHandlerException e) {
+        return e.getMessage();
+    }
+
+    /**
+     * Constructor for IOException.
+     * @param e {@link IOException} the exception.
+     * @return message {@link String} the error message.
+     */
+    @ExceptionHandler(IOException.class)
+    public String handleException(IOException e) {
+        return e.getMessage();
+    }
+
+    /**
+     * Constructor for ClassNotFoundException.
+     * @param e {@link ClassNotFoundException} the exception.
+     * @return message {@link String} the error message.
+     */
+    @ExceptionHandler(ClassNotFoundException.class)
+    public String handleException(Exception e) {
+        return e.getMessage();
+    }
 }
