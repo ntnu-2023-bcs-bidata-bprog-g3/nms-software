1546f71bc32b73fff690f005073975eeccd78260
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-15 13:09:32 +0100

    Fixed init

--- src/main/java/no/ntnu/nms/Init.java
+++ src/main/java/no/ntnu/nms/Init.java
@@ -1,18 +1,12 @@
 package no.ntnu.nms;
 
 import no.ntnu.nms.domainModel.PoolRegistry;
-import no.ntnu.nms.file_handler.FileHandler;
 import no.ntnu.nms.licenseLedger.LicenseLedger;
-import no.ntnu.nms.security.Cryptography;
-import no.ntnu.nms.security.KeyGenerator;
-import org.springframework.util.SerializationUtils;
+
 
 public class Init {
-
     public static void main(String[] args) {
         PoolRegistry.init();
-        byte[] newLedger = Cryptography.xorWithKey(SerializationUtils.serialize("this is a test"), KeyGenerator.KEY);
-        FileHandler.writeToFile(newLedger, "data/temp/license.txt");
         LicenseLedger.init();
     }
 }
