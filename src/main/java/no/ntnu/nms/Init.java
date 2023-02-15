bc91a32e2c17b8b4ae9ee1c104de2e964e033b17
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-15 11:56:46 +0100

    Improved Init for use with ledgers

--- src/main/java/no/ntnu/nms/Init.java
+++ src/main/java/no/ntnu/nms/Init.java
@@ -1,12 +1,20 @@
 package no.ntnu.nms;
 
 import no.ntnu.nms.domainModel.PoolRegistry;
-import no.ntnu.nms.persistence.Controller;
+import no.ntnu.nms.file_handler.FileHandler;
+import no.ntnu.nms.licenseLedger.SubLicenseLedger;
+import no.ntnu.nms.licenseLedger.TopLicenseLedger;
+import no.ntnu.nms.security.Cryptography;
+import no.ntnu.nms.security.KeyGenerator;
+import org.springframework.util.SerializationUtils;
 
 public class Init {
 
     public static void main(String[] args) {
-        PoolRegistry.instance = new PoolRegistry();
-        Controller.savePoolRegAndChecksum();
+        PoolRegistry.init();
+        byte[] newLedger = Cryptography.xorWithKey(SerializationUtils.serialize("this is a test"), KeyGenerator.KEY);
+        FileHandler.writeToFile(newLedger, "data/temp/license.txt");
+        TopLicenseLedger.init();
+        SubLicenseLedger.init();
     }
 }
