64333b16de1dadfea88c21009a8add9347daa987
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-15 13:58:22 +0100

    Cleaned up main

--- src/main/java/no/ntnu/nms/App.java
+++ src/main/java/no/ntnu/nms/App.java
@@ -1,10 +1,6 @@
 package no.ntnu.nms;
 
-import no.ntnu.nms.domainModel.Pool;
-import no.ntnu.nms.domainModel.PoolRegistry;
-import no.ntnu.nms.licenseLedger.LicenseLedger;
 import no.ntnu.nms.logging.Logging;
-import no.ntnu.nms.persistence.PersistenceController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -25,18 +21,5 @@ public class App {
 
         // Starts the API WebServer and hosts the API from the ApiApp class.
         ApplicationContext ctx = SpringApplication.run(App.class, args);
-        System.out.println("Application name: " + ctx.getDisplayName());
-
-        PoolRegistry poolreg = PoolRegistry.getInstance();
-        poolreg.addPool(new Pool("test1", 23, "A tester pool"));
-        poolreg.addPool(new Pool("test2", 23, "A tester pool"));
-        poolreg.addPool(new Pool("test3", 23, "A tester pool"));
-
-        LicenseLedger topledger = LicenseLedger.getInstance();
-
-        //topledger.addLicenseToLedger("data/temp/license.txt");
-        //topledger.addLicenseToLedger("data/temp/license.txt");
-
-        System.out.println(topledger.getLedger());
     }
 }
