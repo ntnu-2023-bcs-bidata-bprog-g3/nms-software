04ff4ec65aa80c7e77c1c6576674c5cdd4cdc7f5
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-17 11:23:50 +0100

    Added CryptographyTest

--- src/test/java/nms/security/CryptographyTest.java
+++ src/test/java/nms/security/CryptographyTest.java
@@ -1,12 +1,44 @@
 package nms.security;
 
+import no.ntnu.nms.security.Cryptography;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CryptographyTest {
 
+    private String message;
+    private byte[] key;
+
+    @BeforeEach
+    public void setUp(){
+        message = "test";
+        key = "1234".getBytes();
+    }
+
     @Test
-    public void TestTest() {
-        assertEquals(1, 1);
+    public void TestEncryption() {
+        byte[] encryptedMessage = null;
+        try {
+            encryptedMessage = Cryptography.xorWithKey(message.getBytes(),key);
+        } catch (Exception ignore) {
+            fail();
+        }
+
+        assertArrayEquals(new byte[]{69, 87, 64, 64}, encryptedMessage);
+    }
+
+    @Test
+    public void TestDecryption(){
+        byte[] decryptedMessage = null;
+        try {
+            decryptedMessage = Cryptography.xorWithKey(new byte[]{69, 87, 64, 64}, key);
+        } catch (Exception e) {
+            fail();
+        }
+
+        assertArrayEquals(message.getBytes(), decryptedMessage);
     }
 }
