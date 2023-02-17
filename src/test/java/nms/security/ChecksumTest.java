8e5be9a3fd9c9c0ae57cc7da12e54b10a2186169
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-17 12:59:27 +0100

    Addec tests

--- src/test/java/nms/security/ChecksumTest.java
+++ src/test/java/nms/security/ChecksumTest.java
@@ -1,12 +1,89 @@
 package nms.security;
 
+import no.ntnu.nms.exception.CryptographyException;
+import no.ntnu.nms.exception.FileHandlerException;
+import no.ntnu.nms.file_handler.FileHandler;
+import no.ntnu.nms.logging.Logging;
+import no.ntnu.nms.security.Checksum;
+import no.ntnu.nms.security.Cryptography;
+import no.ntnu.nms.security.KeyGenerator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ChecksumTest {
 
+    private File file, file2;
+
+    @BeforeAll
+    public static void setUpAll(){
+        try {
+            Logging.setUpLogger("ALL");
+        } catch (IOException ignore) {}
+    }
+    @BeforeEach
+    public void setUp(){
+        file = null;
+        file2 = null;
+        try {
+            file = File.createTempFile("temp", null);
+            file.deleteOnExit();
+            file2 = File.createTempFile("temp", null);
+            file2.deleteOnExit();
+        } catch (IOException ignore){}
+    }
+
     @Test
-    public void TestTest() {
-        assertEquals(1, 1);
+    public void TestGenerateChecksumFromFile() {
+        if (file != null) {
+            try {
+                String checksum = Checksum.generateFromFile(file.getAbsolutePath());
+                assertNotNull(checksum);
+            } catch (CryptographyException e) {
+                fail();
+            }
+        }
+    }
+
+    @Test
+    public void TestCompareChecksum(){
+        if (file != null && file2 != null) {
+            byte[] checksumBytes = Cryptography.xorWithKey(Checksum.generateFromFile(file.getAbsolutePath()).getBytes(), KeyGenerator.KEY);
+            FileHandler.writeToFile(checksumBytes, file2.getAbsolutePath());
+
+            boolean result = Checksum.compare(file.getAbsolutePath(), file2.getAbsolutePath());
+            assertTrue(result);
+        }
+    }
+
+    @Test
+    public void TestCompareChecksumNegative(){
+        if (file != null && file2 != null) {
+            byte[] checksumBytes = Cryptography.xorWithKey("this is a test checksum".getBytes(), KeyGenerator.KEY);
+            FileHandler.writeToFile(checksumBytes, file2.getAbsolutePath());
+
+            boolean result = Checksum.compare(file.getAbsolutePath(), file2.getAbsolutePath());
+            assertFalse(result);
+        }
+    }
+
+
+    @Test
+    public void TestCompareChecksumIllegal(){
+        if (file != null && file2 != null) {
+            assertThrows(FileHandlerException.class, () -> Checksum.compare(file.getAbsolutePath(), file2.getAbsolutePath()));
+        }
+    }
+
+    @Test
+    public void TestCompareChecksumIllegal2(){
+        if (file2 != null) {
+            assertThrows(CryptographyException.class, () -> Checksum.compare("test", file2.getAbsolutePath()));
+        }
     }
 }
