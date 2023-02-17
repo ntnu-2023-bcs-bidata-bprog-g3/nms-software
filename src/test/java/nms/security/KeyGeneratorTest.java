3453ffb43dfe2f7e2e2a332949eecee060a72a52
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-17 11:23:22 +0100

    Added KeyGeneratorTest

--- src/test/java/nms/security/KeyGeneratorTest.java
+++ src/test/java/nms/security/KeyGeneratorTest.java
@@ -1,12 +1,19 @@
 package nms.security;
 
+import no.ntnu.nms.security.KeyGenerator;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class KeyGeneratorTest {
 
     @Test
-    public void TestTest() {
-        assertEquals(1, 1);
+    public void TestEqualKeysKEY() {
+        assertArrayEquals(KeyGenerator.KEY, KeyGenerator.KEY); // KeyGenerator.KEY calls the generateKey() method.
+    }
+
+    @Test
+    public void TestKeyLengthKEY() {
+        byte[] key = KeyGenerator.KEY;
+        assertEquals(32, key.length);
     }
 }
