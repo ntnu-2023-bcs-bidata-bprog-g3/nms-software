a528fcab92ff33b5bd5c106acecf2867738ddd97
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-17 11:21:49 +0100

    Added getter for PropertyChangeSupport

--- src/main/java/no/ntnu/nms/domainModel/Pool.java
+++ src/main/java/no/ntnu/nms/domainModel/Pool.java
@@ -173,6 +173,14 @@ public class Pool implements Serializable {
     }
 
     /**
+     * Get the property change support.
+     * @return {@link PropertyChangeSupport} property change support of the pool.
+     */
+    public PropertyChangeSupport getChanges() {
+        return this.changes;
+    }
+
+    /**
      * Get a string representation of the pool.
      * @return {@link String} string representation of the pool.
      */
