ab9760d453e316fe90a1e096c6e30c91f5c55ca6
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-24 12:34:10 +0100

    Added LfaRegistryException class

---
+++ src/main/java/no/ntnu/nms/exception/LfaRegistryException.java
@@ -0,0 +1,11 @@
+package no.ntnu.nms.exception;
+
+public class LfaRegistryException extends RuntimeException {
+    /**
+     * Constructor for CryptographyException.
+     * @param message the message to be displayed
+     */
+    public LfaRegistryException(String message) {
+        super(message);
+    }
+}
