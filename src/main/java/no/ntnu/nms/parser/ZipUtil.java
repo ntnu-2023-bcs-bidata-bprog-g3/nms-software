a400b35e3b0d98cb898fc0846a2910cba4f6edc0
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-24 08:26:29 +0100

    Removed print statement

--- src/main/java/no/ntnu/nms/parser/ZipUtil.java
+++ src/main/java/no/ntnu/nms/parser/ZipUtil.java
@@ -23,7 +23,6 @@ public class ZipUtil {
     public static String unzipper(ZipInputStream inputStream) throws ParserException, IOException {
         String dirName = String.valueOf(Math.abs(((int) Math.pow(10, 8))
                 + (new Random().nextInt(9 * (int) Math.pow(10, 8)))));
-        System.out.println("Dirname: " + dirName);
         final Path dir = Path.of("data/temp/" + dirName);
         for (ZipEntry entry; (entry = inputStream.getNextEntry()) != null; ) {
             try {
