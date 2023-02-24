6028aa2bc39782c7b7fbcd9cdf93897237cc4437
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-24 08:25:45 +0100

    Added pub key to code

--- src/main/java/no/ntnu/nms/parser/LicenseParser.java
+++ src/main/java/no/ntnu/nms/parser/LicenseParser.java
@@ -23,6 +23,16 @@ public class LicenseParser {
 
     private String dirName;
 
+    private final String publicKey = "-----BEGIN PUBLIC KEY-----\n" +
+            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvP769XecV3Z0E2BC19Vo\n" +
+            "wu4z4OkVxjnfXFZiqLwD686W69vRDjn1ZCUphLeSB/x/5bhkiJHSQSJVDUgYBhzo\n" +
+            "i2itFpEIrweuaq1sSiQjRcnv+UV9XD7BU1JmR4zZQb8xyrbQeXsKhN6gT3Q0/nmt\n" +
+            "j4PF0fiRKeuA8pW618ftbjraSY/cgr8C/x3A1GcJBMvKMVGgZrQSTxZViZN0WdcP\n" +
+            "p+jG70s4G5jL35B8NdlafiW7pYaE4A8VUHcHIvdrkJWPNOwiTPa2CPRtopjZ1b83\n" +
+            "uPkDxVUSjMKPNkcDyXI4w/n4JwUNdWrgLHvTCeMOJWQheOM22+RuiEqA+z2fnvS2\n" +
+            "0QIDAQAB\n" +
+            "-----END PUBLIC KEY-----\n";
+
     public LicenseParser() {
         licenseFile = null;
         signatureFile = null;
