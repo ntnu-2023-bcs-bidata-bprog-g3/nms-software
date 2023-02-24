b57cea2e4a9fa5f61b4a497d6df21fb9e14a2076
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-02-24 14:20:21 +0100

    Added LicenseGeneratorException

---
+++ src/main/java/no/ntnu/nms/exception/LicenseGeneratorException.java
@@ -0,0 +1,11 @@
+package no.ntnu.nms.exception;
+
+public class LicenseGeneratorException extends RuntimeException {
+    /**
+     * Constructor for CryptographyException.
+     * @param message the message to be displayed
+     */
+    public LicenseGeneratorException(String message) {
+        super(message);
+    }
+}
