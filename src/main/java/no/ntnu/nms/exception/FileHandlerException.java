45e36e98d258346d7ee5b3d1656fdcc48718923c
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-03-24 12:34:51 +0100

    Fixed checkstyle issues

--- src/main/java/no/ntnu/nms/exception/FileHandlerException.java
+++ src/main/java/no/ntnu/nms/exception/FileHandlerException.java
@@ -9,6 +9,6 @@ public class FileHandlerException extends RuntimeException {
      * @param message the message to be displayed
      */
     public FileHandlerException(String message) {
-            super(message);
-        }
+        super(message);
+    }
 }
