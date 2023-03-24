a7ea920eb0d8c57c91e3756f7b95ea38d4d2e0f3
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-03-24 11:25:59 +0100

    Added root public key to constants

--- src/main/java/no/ntnu/nms/CustomerConstants.java
+++ src/main/java/no/ntnu/nms/CustomerConstants.java
@@ -6,4 +6,14 @@ public class CustomerConstants {
 
     public final static String TEST_DATA_PATH = "test_files/";
     public final static String PROD_DATA_PATH = "data/";
+
+    public final static String ROOT_PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----\n" +
+            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvP769XecV3Z0E2BC19Vo\n" +
+            "wu4z4OkVxjnfXFZiqLwD686W69vRDjn1ZCUphLeSB/x/5bhkiJHSQSJVDUgYBhzo\n" +
+            "i2itFpEIrweuaq1sSiQjRcnv+UV9XD7BU1JmR4zZQb8xyrbQeXsKhN6gT3Q0/nmt\n" +
+            "j4PF0fiRKeuA8pW618ftbjraSY/cgr8C/x3A1GcJBMvKMVGgZrQSTxZViZN0WdcP\n" +
+            "p+jG70s4G5jL35B8NdlafiW7pYaE4A8VUHcHIvdrkJWPNOwiTPa2CPRtopjZ1b83\n" +
+            "uPkDxVUSjMKPNkcDyXI4w/n4JwUNdWrgLHvTCeMOJWQheOM22+RuiEqA+z2fnvS2\n" +
+            "0QIDAQAB\n" +
+            "-----END PUBLIC KEY-----\n";
 }
