1b5114efaa9d128676ee560fe837842a7db72451
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-03-24 12:34:14 +0100

    Added docs

--- src/main/java/no/ntnu/nms/exception/ExceptionHandler.java
+++ src/main/java/no/ntnu/nms/exception/ExceptionHandler.java
@@ -1,7 +1,15 @@
 package no.ntnu.nms.exception;
 
+/**
+ * Class for handling exceptions, for removing illegal characters.
+ */
 public class ExceptionHandler {
 
+    /**
+     * Method for removing illegal characters from an exception message.
+     * @param e the exception
+     * @return the exception message without illegal characters
+     */
     public static String getMessage(Exception e) {
         return e.getMessage().replace("\"", "'");
     }
