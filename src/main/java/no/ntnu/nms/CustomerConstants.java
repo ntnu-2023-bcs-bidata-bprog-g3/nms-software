dcac711abd659f758f68b4e347a5f27433199704
Sander Osvik Brekke <sanderob@stud.ntnu.no>
2023-03-24 11:25:26 +0100

    Fixed customer Name

--- src/main/java/no/ntnu/nms/CustomerConstants.java
+++ src/main/java/no/ntnu/nms/CustomerConstants.java
@@ -2,7 +2,7 @@ package no.ntnu.nms;
 
 public class CustomerConstants {
 
-    public final static String CUSTOMER_NAME = "TestCustomer";
+    public final static String CUSTOMER_NAME = "DevelopmentCustomer";
 
     public final static String TEST_DATA_PATH = "test_files/";
     public final static String PROD_DATA_PATH = "data/";
