befa3a69918e85d123b23e59f9dfa4e5e462f5bf
Ivan Norderhaug <ivan_norderhaug@outlook.com>
2023-05-03 13:55:38 +0200

    delete unused method Cryptography.xorWithKey

--- src/main/java/no/ntnu/nms/security/Cryptography.java
+++ src/main/java/no/ntnu/nms/security/Cryptography.java
@@ -41,24 +41,6 @@ public class Cryptography {
     }
 
     /**
-     * XORs a byte array with a key.
-     * @param input The byte array to XOR.
-     * @param key The key to XOR with.
-     * @return The XORed byte array.
-     */
-    public static byte[] xorWithKey(byte[] input, byte[] key) throws CryptographyException {
-        if (input == null) {
-            throw new CryptographyException("Failed to decrypt/encrypt: input is null");
-        }
-
-        byte[] output = new byte[input.length];
-        for (int i = 0; i < input.length; i++) {
-            output[i] = (byte) (input[i] ^ key[i % key.length]);
-        }
-        return output;
-    }
-
-    /**
      * Encrypts/decrypts a message using AES encryption.
      * @param message The message to encrypt.
      * @param mode The mode to use, e.g. ENCRYPT or DECRYPT
